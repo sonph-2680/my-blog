361c1a8510d1ccd5814ef81ec527dbacd6dc7066
Sownbanana <s.v.o.a.26@gmail.com>
2021-10-05 08:43:29 +0700

    chore: change home style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 {{-- {{ dd(get_defined_vars()['__data']) }} --}}
-<div class="d-flex justify-content-between">
-    <h4 class="d-flex align-items-center font-weight-bold">Post List</h4>
+<div class="d-flex justify-content-end justify-content-sm-between">
+    <h4 class="d-none d-sm-flex align-items-center font-weight-bold">Post List</h4>
     <nav class="navbar navbar-light bg-light pr-0">
-        <form class="form-inline" action="{{ route('home')}}">
-            <input class="form-control mr-sm-2" name="search" type="search" placeholder="Search" aria-label="Search" value="{{request()->get('search')}}">
-            <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">
+        <form class="d-flex" action="{{ route('home')}}">
+            <input class="form-control mr-2" name="search" type="search" placeholder="Search" aria-label="Search" value="{{request()->get('search')}}">
+            <button class="btn btn-outline-primary my-0" type="submit">
                 <i class="fa fa-search" aria-hidden="true"></i>
             </button>
         </form>
